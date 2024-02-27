e98cf589d56267308f2f18ce657d2fbe55d3c8e2
D1ceS5 <60752639+D1ceS5@users.noreply.github.com>
2024-02-27 14:33:56 +0200

    Fix "not implemented"

--- src/components/SidebarButton/SidebarButton.jsx
+++ src/components/SidebarButton/SidebarButton.jsx
@@ -1,7 +1,7 @@
 import './sidebarButton.scss'
 
 function SidebarButton({ text, icon, active,onClick }) {
-    return <div className={active? "sidebarButton active": "sidebarButton"}>
+    return <div className={active? "sidebarButton active": "sidebarButton"} onClick={onClick}>
         <img className='sidebarButton-icon' src={icon} />
         <span className='sidebarButton-text' >{text}</span>
     </div>
