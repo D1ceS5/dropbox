f591445f30e076ec974ed4a86ea77d0108dbca78
D1ceS5 <60752639+D1ceS5@users.noreply.github.com>
2024-02-27 14:32:51 +0200

    Added not implemented message

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,6 +4,11 @@ import { GlobalContext } from "../../context"
 import { useContext } from "react"
 
 function Sidebar() {
+
+    function notImplemented(){
+        alert("Not implemented")
+    }
+
     let { menuState,toggleMenu } = useContext(GlobalContext)
     return <>
         <div className={menuState ? "sidebar active" : "sidebar"}>
@@ -13,9 +18,9 @@ function Sidebar() {
             </div>
             <div>
                 <SidebarButton active={true} icon={'../../assets/Sidebar/all-files.svg'} text={"All files"} />
-                <SidebarButton icon={'../../assets/Sidebar/photos.svg'} text={"Photos"} />
-                <SidebarButton icon={'../../assets/Sidebar/shared.svg'} text={"Shared"} />
-                <SidebarButton icon={'../../assets/Sidebar/signatures.svg'} text={"Signatures"} />
+                <SidebarButton onClick={notImplemented} icon={'../../assets/Sidebar/photos.svg'} text={"Photos"} />
+                <SidebarButton onClick={notImplemented} icon={'../../assets/Sidebar/shared.svg'} text={"Shared"} />
+                <SidebarButton onClick={notImplemented} icon={'../../assets/Sidebar/signatures.svg'} text={"Signatures"} />
             </div>
 
         </div>
